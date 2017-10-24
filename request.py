29ddd7ac19c7d9b3ca4e9aa79cea85f71936e1b2
jainnikhil30 <jainnikhil30@gmail.com>
2017-10-24 17:13:54 +0530

    Update request.py

--- request.py
+++ request.py
@@ -4,7 +4,7 @@ import json
 import requests
 
 def main():
-  res=requests.get('https://raw.githubusercontent.com/michelleperz/inventory_presentation/master/custom.json')
+  res=requests.get('https://raw.githubusercontent.com/jainnikhil30/custom_inv/master/custom.json')
   data=res.json()
   print json.dumps(data, sort_keys=True, indent=2)
 if __name__ == '__main__':
