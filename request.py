fb96534f466d792e48ccbcf21eaabf7462d39809
jainnikhil30 <jainnikhil30@gmail.com>
2020-03-27 16:39:17 +0530

    Update request.py

--- request.py
+++ request.py
@@ -2,8 +2,10 @@
 
 import json
 import requests
+import time
 
 def main():
+  time.sleep(200)
   res=requests.get('https://raw.githubusercontent.com/jainnikhil30/custom_inv/master/custom.json')
   data=res.json()
   print json.dumps(data, sort_keys=True, indent=2)
