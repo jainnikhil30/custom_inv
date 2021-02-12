94c743f335523df45a0bb8a9da038e8f8d6eb49c
jainnikhil30 <jainnikhil30@gmail.com>
2021-02-12 12:57:31 +0530

    Update request.py

--- request.py
+++ request.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import json
 import requests
@@ -6,9 +6,9 @@ import time
 
 def main():
   # sleep
-  time.sleep(1200)
+  #time.sleep(1200)
   res=requests.get('https://raw.githubusercontent.com/jainnikhil30/custom_inv/master/custom.json')
   data=res.json()
-  print json.dumps(data, sort_keys=True, indent=2)
+  print(json.dumps(data, sort_keys=True, indent=2))
 if __name__ == '__main__':
   main()
